2b9ea46f0308e15d13f0e07a0861823dfe113297
PBustamante65 <136853530+PBustamante65@users.noreply.github.com>
2026-07-14 12:33:12 -0600

    Update geodata.py

--- Code/geodata.py
+++ Code/geodata.py
@@ -26,10 +26,6 @@ print(f"Hay {len(df)} tiendas con coordenadas")
 
 def carto(lons, lats):
 
-    # Longitude, Latitude
-    # lons = [-122.4194, -74.0060, 2.3522]  # SF, NYC, Paris
-    # lats = [37.7749, 40.7128, 48.8566]
-
     fig, ax = plt.subplots(subplot_kw={"projection": ccrs.PlateCarree()})
     ax.scatter(lons, lats, color="red", transform=ccrs.PlateCarree())
     ax.coastlines()
